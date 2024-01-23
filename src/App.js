2f92a8d354ec7586243a246646a1f301705566c4
cusmoritz <cus.moritz@gmail.com>
2024-01-22 20:04:46 -0700

    lets go

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
 
 
 const App = () => {
 return (
-    <>
-        <div>
-    <h1>TIME</h1>
-    <div>Body?</div>
-    </div>
-    </>
+    <BrowserRouter>
+        <div className="App">
+            <Routes>
+            <Route exact path="/" element={<Home/>} />
 
-);
+            </Routes>
+        </div>
+    </BrowserRouter>
+    );
 
 };
 

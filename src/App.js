59902db4c8627cfb97fcdfee53c0f20bfdaebbdf
cusmoritz <cus.moritz@gmail.com>
2024-01-22 20:01:48 -0700

    lets start building

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const App = () => {
+return (
+    <>
+        <div>
+    <h1>TIME</h1>
+    <div>Body?</div>
+    </div>
+    </>
+
+);
+
+};
+
+
+export default App;

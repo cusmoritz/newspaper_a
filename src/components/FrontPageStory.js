9bc96e4c380c805a3ab360a6990f1f9a3dc47c61
Marcus Moritz <cus.moritz@gmail.com>
2024-02-23 17:01:58 -0700

    chore: front page styling

--- src/components/FrontPageStory.js
+++ src/components/FrontPageStory.js
@@ -8,14 +8,15 @@ export const FrontPageStory = ({props}) => {
             <h2 className="front-page-headline" value={story.story_id}>{story.story_head}</h2>
             <h3 className="front-page-story-deck">{story.story_deck}</h3>
             <div className="front-page-author-container">
-                <p className="front-page-author-name">{story.story_author.author_name}</p>
+                <p className="front-page-author-name">By {story.story_author.author_name}</p>
                 <p className="front-page-author-email">{story.story_author.author_email}</p>
             </div>
             <p className="front-page-story-led">{story.story_led} {story.story_led}</p>
             <fieldset className="front-page-tag-container">
+                <p>Tags: &nbsp;</p> 
                 {story.story_tags.map((tag) => {
                     return (
-                        <p>{tag}</p>
+                        <a>#{tag}</a>
                     )
                 })}
             </fieldset>

50ed147b93a41f466f425fb2171e1fad7c517b78
cusmoritz <cus.moritz@gmail.com>
2024-04-03 16:50:30 -0600

    chore: primary catagory page start

--- src/components/PrimaryCatPage.js
+++ src/components/PrimaryCatPage.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export const PrimaryCatPage = () => {
 
     const {primaryCat} = useParams();
-    console.log(primaryCat)
+    const primary = primaryCat.toUpperCase();
+
+    const [stories, setStories] = useState([]);
+
+    const fetchPrimaryStories = async () => {
+        console.log('fetching ... ')
+    };
+    
+    const loadPage = async () => {
+        const mainCatStories = await fetchPrimaryStories();
+        if (mainCatStories) {
+            setStories(mainCatStories);
+        };
+    };
+
+    useEffect(() => {
+        // loadPage();
+    }, []);
     return (
-        <>
-        This is the primary catagory page for {primaryCat}.
-        </>
+        <div className="primary-catagory-page-container">
+            <h3 className="primary-catagory-header">{primary}</h3>
+        </div>
     )
 }

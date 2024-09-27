dcc027471c15f42ded6814a0898f9439b6159ce1
cusmoritz <cus.moritz@gmail.com>
2024-09-26 21:23:56 -0600

    feat: search options init

--- src/components/admin/SearchFiltersComponentMono.js
+++ src/components/admin/SearchFiltersComponentMono.js
@@ -3,8 +3,40 @@ import React from "react";
 export const SearchFilterComponentMono = () => {
 
     return (
-        <>
+        <div>
         Search filters component.
-        </>
+        <p></p>
+        <label htmlFor="date-search">Search stories by date published:</label>
+        <input className="date-search" type="date" /> &nbsp;
+        <button>Search</button>
+        <p></p>
+        <label htmlFor="include-date">Include date in keyword search:</label>
+        <input className="include-date" type="checkbox" />
+        <p></p>
+        <label htmlFor="include-date-range">Include date range in keyword search:</label>
+        <input className="include-date-range" type="checkbox" />
+        <p></p>
+        <label htmlFor="range-search">Search by date range:</label>
+        <input className="range-search" type="date" /> &nbsp;
+        <input className="range-search" type="date" /> &nbsp;
+        <button>Search</button>
+        <p></p>
+        <label htmlFor="author-search">Search by author:</label>
+        <select>
+            <option value={""}>Select an option</option>
+            {/* import all the authors here */}
+        </select> &nbsp;
+        <button>Search</button>
+        <p></p>
+        <label htmlFor="headline-search">Headline keyword search</label>
+        <input className="headline-search" type="text" /> &nbsp;
+        <button>Search</button>
+        <p>Note: Headline keyword search can take up to a minute or more.</p>
+        <p></p>
+        <label htmlFor="body-text-search">Body text search</label>
+        <input className="body-text-search" type="text" />
+        <p>Note: Body text search can take several minutes. To make it faster, include a date range.</p>
+        <p></p>
+        </div>
     )
 }
--- src/components/admin/SearchPageMono.js
+++ src/components/admin/SearchPageMono.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { SearchFilterComponentMono } from "./SearchFiltersComponentMono";
 
 export const SearchPageMono = () => {
@@ -9,7 +10,8 @@ export const SearchPageMono = () => {
     return (
         <div>
             <h2>Advanced Story Search</h2>
-
+            <button><Link to={'/'}>Back</Link></button>
+            <p></p>
             {searchBool ? null : 
             <SearchFilterComponentMono />
             }

1e014db34b007206971223c19e2e84188f8641cc
cusmoritz <cus.moritz@gmail.com>
2025-01-11 14:10:09 -0700

    feaT: image upload feature changes

--- src/components/admin/ImageUpload.js
+++ src/components/admin/ImageUpload.js
@@ -9,47 +9,37 @@ export const ImageUpload = () => {
     // let file = document.querySelector('#file');
     let app = document.querySelector('#image-preview');
 
-    const [photo, setPhoto] = useState(undefined);
+    const [photos, setPhotos] = useState([]);
 
-    function handleSubmit (event) {
-        event.preventDefault();
-        console.log(event)
-
-        const url = URL.createObjectURL(event.target.files[0]); // this does give us the url
-        console.log('url', url)
-        setPhoto(url);
-
-        // Create a new FileReader() object
-        let reader = new FileReader();
-        reader.readAsDataURL(event.target.files[0]) // this is the Blob
-        // Setup the callback event to run when the file is read
-        reader.onload = function() {
-            console.log(reader.result);
-        };
-        reader.onerror = function() {
-            console.log(reader.error);
-        };
-
+    function handleAddPhotos (e) {
+        const file = e.target.files;
+        if (file && file.length > 0) {
+            var image = URL.createObjectURL(e.target.files[0])
+            setPhotos([...photos, image]);
+          }
     }
 
-    // function logFile (event) {
-    //     // let str = event.target.result;
-    //     // let img = document.createElement('img');
-    //     // img.src = str;
-    //     // app.append(img);
-    //     console.log(event);
-    // }
+    const handlePhotoSubmit = () => {
+        if (photos.length > 0) {
+            console.log('photos: ', photos)
+            // send photos to db
+        }
+    }
 
     return (
         <>
         <form id="upload">
             <label htmlFor="file">File to upload</label>
-            <input type="file" id="file" accept="image/*" onChange={(event) => handleSubmit(event)}/>
-
-            {/* <button onSubmit={(event) => handleSubmit(event)}>Upload</button> */}
+            <input type="file" id="file" accept="image/*" onChange={handleAddPhotos}/>
         </form>
-        {!photo ? null : <img src={photo} />}
-
+        {!photos ? null : 
+        photos.map((image) => {
+            return (
+                <img src={image}></img>
+            )
+        })
+        }
+        <button onClick={handlePhotoSubmit}>Submit Photos</button>
         </>
         
     )

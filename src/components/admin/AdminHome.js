068a3533818715dbb7272a5654fddb02c1a7b6ee
cusmoritz <cus.moritz@gmail.com>
2025-01-11 14:24:11 -0700

    chore: admin home style

--- src/components/admin/AdminHome.js
+++ src/components/admin/AdminHome.js
@@ -14,22 +14,25 @@ export const AdminHome = (props) => {
     return (
         <div className="admin-home-container">
             <h1>Admin page</h1>
-            {/* {!props.globalBreakingBool ? null : 
-            <div>
-                <p>There is breaking news.</p>
+            <div className="admin-button-container">
+                {/* {!props.globalBreakingBool ? null : 
+                <div>
+                    <p>There is breaking news.</p>
+                </div>
+                } */}
+                <button><Link to="/createstory">New story</Link></button>
+                <button><Link to="/storystats">Story stats</Link></button>
+                <button><Link to="/authorpage">Author Page</Link></button>
+                <button><Link to="/image-upload">Image Upload</Link></button>
+                <button><Link to="/sources">Check Sources</Link></button>
+                <button><Link to="/resources">Manage / View Community Resources</Link></button>
+                <button><Link to="/search">Advanced Story Search</Link></button>
+                <button><Link to="/page-views">Story Page Views</Link></button>
+                {/* <p></p>
+                <label htmlfFor="search">StorySearch:</label>
+                <input className="search"/> */}
             </div>
-            } */}
-            <button><Link to="/createstory">New story</Link></button>
-            <button><Link to="/storystats">Story stats</Link></button>
-            <button><Link to="/authorpage">Author Page</Link></button>
-            <button><Link to="/image-upload">Image Upload</Link></button>
-            <button><Link to="/sources">Check Sources</Link></button>
-            <button><Link to="/resources">Manage / View Community Resources</Link></button>
-            <button><Link to="/search">Advanced Story Search</Link></button>
-            <button><Link to="/page-views">Story Page Views</Link></button>
-            {/* <p></p>
-            <label htmlfFor="search">StorySearch:</label>
-            <input className="search"/> */}
         </div>
+
     );
 };
